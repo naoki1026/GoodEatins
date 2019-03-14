6041656c406cd35df7abde40b821170b7a2affb4
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-03-14 17:46:19 +0900

    Initial Commit

---
+++ GoodEatins2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins2
+//
+//  Created by Naoki Arakawa on 2019/03/14.
+//  Copyright © 2019 Naoki Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
